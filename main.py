7039129298fde6be30ddc98b2872ae587ce262f0
Ismail Ait El Kamel <97767629+ismaelaek@users.noreply.github.com>
2024-12-06 18:10:40 +0100

    Added arabic description and update genres

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import random
 import telebot 
 from telebot import types
 from decouple import config
+from googletrans import Translator
 import os
 
-# the following variables are stored in a ".env " file 
-api_key = config('api_key')
-bot_username = config('bot_username')
-bot_token = config('bot_token')
-email = config('email')
-owner_name = config('owner_name')
-GitHub = config('GitHub')
+translator = Translator()
 
-# get all availables genres and store them in a variable later 
+# the following variables are stored in a ".env " file 
+api_key = config('API_KEY')
+bot_username = config('BOT_USERNAME')
+bot_token = config('BOT_TOKEN')
+email = config('EMAIL')
+owner_name = config('OWNER_NAME')
+GitHub = config('GITHUB')
+
+# get all available genres and store them in a variable later
 def get_all_genres():
     url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}"
     response = requests.get(url)
@@ -23,27 +26,28 @@ def get_all_genres():
 
 all_genres = get_all_genres()
 # set a mood for each genre 
-# it's much easier to make only one dictionnary
+# it's much easier to make only one dictionary
 moods = [
-    {"Excited": "Action"},
-    {"Thrilled": "Adventure"},
-    {"Enchanting": "Animation"},
+    {"Excited": "Adventure"},
+    {"Thrilled": "Science Fiction"},
+    {"Enchanting": "Fantasy"},
     {"Hilarious": "Comedy"},
-    {"Intrigued": "Crime"},
+    {"Intrigued": "Mystery"},
     {"Enlightened": "Documentary"},
     {"Thoughtful": "Drama"},
     {"Joyful": "Family"},
-    {"Imaginative": "Fantasy"},
+    {"Imaginative": "Animation"},
     {"Fascinated": "History"},
-    {"Scared": "Horror"},
+    {"Scared": "Comedy"},
     {"Melodious": "Music"},
-    {"Curious": "Mystery"},
+    {"Curious": "Documentary"},
     {"Romantic": "Romance"},
-    {"Wondrous": "Science Fiction"},
-    {"Entertained": "TV Movie"},
-    {"Intense": "Thriller"},
+    {"Wondrous": "Fantasy"},
+    {"Entertained": "Action"},
+    {"Intense": "Drama"},
     {"Heroic": "War"},
-    {"Adventurous": "Western"}
+    {"Adventurous": "Western"},
+    {"Sad": "Comedy"}
 ]
 
 ###############
@@ -52,7 +56,7 @@ moods = [
 bot = telebot.TeleBot(bot_token)
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Welcome to Movie Recommaneder bot \n\n /help for help ")
+    bot.send_message(message.chat.id, "Welcome to Movie Recommender bot \n\n /help for help ")
 
 @bot.message_handler(commands=['help'])
 def start(message):
@@ -62,22 +66,22 @@ def start(message):
 @bot.message_handler(commands=['contact'])
 def handle_contact(message):
     info = f"""
-        Owner Name  -  {owner_name}
-        Email       -  {email} 
-        GitHub Profile       -  {GitHub} 
+Name: {owner_name}
+Email: {email} 
+GitHub: {GitHub} 
     """
     bot.send_message(chat_id=message.chat.id, text=info)
 
 @bot.message_handler(commands=['mood'])
-def Choose_Mode(message):
+def choose_mood(message):
     # Create a menu keyboard
     keyboard = types.InlineKeyboardMarkup(row_width=3)
     
     # Add buttons for each mood
     for mood in moods:
-        Current_Mood = list(mood.keys())[0]
-        Selected_Genre = mood[Current_Mood]
-        button = types.InlineKeyboardButton(text=Current_Mood, callback_data=Selected_Genre)
+        current_mood = list(mood.keys())[0]
+        selected_genre = mood[current_mood]
+        button = types.InlineKeyboardButton(text=current_mood, callback_data=selected_genre)
         keyboard.add(button)
     
     # Send the menu message
@@ -87,37 +91,38 @@ def Choose_Mode(message):
 @bot.callback_query_handler(func=lambda call: True)
 def button_click(call):
     
-    Selected_Genre = call.data.strip()
-    bot.send_message(chat_id=call.message.chat.id, text=f"we have chosen {Selected_Genre} movies/series for your current mood !")
+    selected_genre = call.data.strip()
+    bot.send_message(chat_id=call.message.chat.id, text=f"we have chosen {selected_genre} movies/series for your current mood !")
     
-    # get id of selectedd genre 
-    Genre_id = all_genres.get(Selected_Genre)
+    # get id of selected genre
+    genre_id = all_genres.get(selected_genre)
     
     # make a request to TMDb to return movies depends on chosen mood
-    if Genre_id:
+    if genre_id:
         response = requests.get(f'https://api.themoviedb.org/3/discover/movie', params={
             'api_key': {api_key},
-            'with_genres': {Genre_id},
+            'with_genres': {genre_id},
             'sort_by': 'popularity.desc',
             'page': 1
         }).json()
         movies = response['results']
         sorted_movies = sorted(movies, key=lambda x: x['popularity'], reverse=True)
         
-        # make a list of top 15 movie and choose a randam one 
-        top_movies = sorted_movies[:15]
+        # make a list of top 20 movie and choose a random one
+        top_movies = sorted_movies[:20]
+        print(top_movies)
         random_movie = random.choice(top_movies)
-        
-        # extarcting selected movie's info
+
+        # extracting selected movie's info
         title = random_movie['title']
         poster_path = random_movie['poster_path']
-        description = random_movie['overview']
+        overview = random_movie['overview']
+        arabic_overview = translator.translate(overview, src='en', dest='ar')
         rating = random_movie['vote_average']
-        story = random_movie['tagline'] if 'tagline' in random_movie else 'N/A'
-        imgurl='https://image.tmdb.org/t/p/original' + poster_path
+        img_url='https://image.tmdb.org/t/p/original' + poster_path
         
-        # I have a problem sending photo by url so I had to make the program download the poster and send it then delete it 
-        photo_data = requests.get(imgurl)
+        # I have a problem sending photo by url, so I had to make the program download the poster and send it then delete it
+        photo_data = requests.get(img_url)
         photo_filename = 'photo.jpg'
         with open(photo_filename, 'wb') as photo_file:
             photo_file.write(photo_data.content)
@@ -128,7 +133,7 @@ def button_click(call):
         # Delete the downloaded photo file
         os.remove(photo_filename)
         
-        caption=f'{title}\n\nRating: {rating}\n\nDescription: {description}\n\nStory: {story}'
+        caption=f' **{title}** \n\nRating: {rating}\n\nOverview: {overview}\n\nبالعربية: {arabic_overview.text}'
         bot.send_message(chat_id=call.message.chat.id, text=caption)
 
 # start the bot 
